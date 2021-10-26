f1dc2e14dfefc3cc9b22840f3c11b5fb5fc1d438
Nhahan <kisy324@naver.com>
2021-10-27 00:19:14 +0900

    regex parsing

--- src/main.js
+++ src/main.js
@@ -14,9 +14,18 @@ const http = require("http")
  * POST /posts
  */
 const server = http.createServer((req, res) => {
+  // capture group ()
+  const POSTS_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_]+)$/
   if (req.url === "/posts" && req.method === "GET") {
-  } else if (req.url && /^\/posts\/[a-zA-Z0-9-_]+$/.test(req.url)) {
+    res.statusCode = 200
+  } else if (req.url && POSTS_ID_REGEX.test(req.url)) {
+    const regexResult = POSTS_ID_REGEX.exec(req.url)
+    if (regexResult) {
+      const postId = regexResult[1]
+    }
+    res.statusCode = 200
   } else if (req.url === "/posts" && req.method === "POST") {
+    res.statusCode = 200
   } else {
     res.statusCode = 404
     res.end("Not found.")
