7f58e753b0341ef0388b9655304e2ddfe96408ad
Nhahan <kisy324@naver.com>
2021-10-27 00:23:46 +0900

    refactoring regexResult

--- src/main.js
+++ src/main.js
@@ -14,18 +14,23 @@ const http = require("http")
  * POST /posts
  */
 const server = http.createServer((req, res) => {
-  // capture group ()
-  const POSTS_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_]+)$/
+  const POSTS_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_]+)$ / // capture group ()
+  const postIdRegexResult =
+    (req.url && POSTS_ID_REGEX.exec(req.url)) || undefined
+
   if (req.url === "/posts" && req.method === "GET") {
     res.statusCode = 200
-  } else if (req.url && POSTS_ID_REGEX.test(req.url)) {
-    const regexResult = POSTS_ID_REGEX.exec(req.url)
-    if (regexResult) {
-      const postId = regexResult[1]
-    }
+    res.end("200")
+  } else if (postIdRegexResult) {
+    // GET /posts:id
+    const postId = postIdRegexResult[1]
+    console.log(postId)
+
     res.statusCode = 200
+    res.end("200")
   } else if (req.url === "/posts" && req.method === "POST") {
     res.statusCode = 200
+    res.end("200")
   } else {
     res.statusCode = 404
     res.end("Not found.")
