93002f57f55bea107c0b7943c5db944ee455e966
Nhahan <kisy324@naver.com>
2021-10-27 22:06:35 +0900

    posts.push

--- src/main.js
+++ src/main.js
@@ -44,19 +44,22 @@ const server = http.createServer((req, res) => {
       post: posts.map((post) => ({
         id: post.id,
         title: post.title,
+        content: post.content,
       })),
       totalCount: posts.length,
     }
 
     res.statusCode = 200
+    res.setHeader("Content-Type", "application/json; charset=utf-8")
     res.end(JSON.stringify(result))
-  } else if (postIdRegexResult) {
+  } else if (postIdRegexResult && req.method === "GET") {
     // GET /posts:id
     const postId = postIdRegexResult[1]
     const post = posts.find((_post) => _post.id === postId)
 
     if (post) {
       res.statusCode = 200
+      res.setHeader("Content-Type", "application/json; charset=utf-8")
       res.end(JSON.stringify(post))
     } else {
       res.statusCode = 404
@@ -66,6 +69,24 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.end("200")
   } else if (req.url === "/posts" && req.method === "POST") {
+    req.setEncoding("utf-8")
+    req.on("data", (data) => {
+      /**
+       * @typedef CreatePostBody
+       * @property {string} title
+       * @property {string} content
+       */
+
+      /** @type {CreatePostBody} */
+      const body = JSON.parse(data)
+
+      posts.push({
+        id: body.title.toLocaleLowerCase().replace(/\s/g, "_"),
+        title: body.title,
+        content: body.content,
+      })
+    })
+
     res.statusCode = 200
     res.end("200")
   } else {
