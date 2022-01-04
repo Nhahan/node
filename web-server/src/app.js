e6d4dcb9aa1bebc6dc930861cdfd9dca9e778195
sun <sun@codebrick.co>
2022-01-04 20:30:21 +0900

    server test

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,3 +1,11 @@
 const express = require("express");
 
 const app = express();
+
+app.get("", (req, res) => {
+    res.send("Hello express!");
+});
+
+app.listen(3005, () => {
+    console.log("Server is up on port 3000.");
+});
