2dc0ee5c21604cba1d4ceb72ef7253018fabba5a
sun <sun@codebrick.co>
2022-01-04 17:46:59 +0900

    diffrent datas between JSON.strigify and JSON.parse

--- playground/1-json.js
+++ playground/1-json.js
@@ -4,4 +4,7 @@ const book = {
 };
 
 const bookJSON = JSON.stringify(book);
-console.log(bookJSON);
+console.log(bookJSON, bookJSON.author);
+
+const parsedData = JSON.parse(bookJSON);
+console.log(parsedData, parsedData.author);
