e11ea8c870efb1bdafac2a0115324d69f146c489
sun <sun@codebrick.co>
2022-01-04 20:37:40 +0900

    gitguard

--- weather-app/app.js
+++
@@ -1,7 +0,0 @@
-const request = require("postman-request");
-
-const url =
-    "http://api.weatherstack.com/current?access_key=e4a2352f7c18c1a6ec8fb40cea43cca7&query=New%20York";
-request({ url: url, json: true }, (error, response) => {
-    console.log(response.body.current);
-});
