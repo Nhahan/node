4592e7c64aa3a837c837f233f66b5af134155ee4
sun <sun@codebrick.co>
2022-01-04 20:33:13 +0900

    route test

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,6 +6,18 @@ app.get("", (req, res) => {
     res.send("Hello express!");
 });
 
+app.get("/help", (req, res) => {
+    res.send("Help page");
+});
+
+app.get("/about", (req, res) => {
+    res.send("About");
+});
+
+app.get("/weather", (req, res) => {
+    res.send("Perfect weather");
+});
+
 app.listen(3005, () => {
     console.log("Server is up on port 3000.");
 });
