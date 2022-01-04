a89aecef579889f0f307ef31bde6ea78bc7a6467
sun <sun@codebrick.co>
2022-01-04 21:47:01 +0900

    set

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,6 +7,7 @@ console.log(path.join(__dirname, "../public"));
 const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
 
 app.get("/weather", (req, res) => {
