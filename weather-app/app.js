2840ebf861106a4fec0927ecfafa6db03d6cb520
sun <sun@codebrick.co>
2022-01-04 19:37:11 +0900

    request weather

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,6 @@ const request = require("postman-request");
 
 const url =
     "http://api.weatherstack.com/current?access_key=e4a2352f7c18c1a6ec8fb40cea43cca7&query=New%20York";
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.current);
 });
