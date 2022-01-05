8f69992fe6260b54b59adfcd02868cc94d7af874
sun <sun@codebrick.co>
2022-01-05 18:27:37 +0900

    base

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const routers = require("../routers");
+// const routers = require("../routers");
 const axios = require("axios").default;
 const http = require("http");
 const cors = require("cors");
@@ -11,31 +11,30 @@ app.use(cors({ origin: true, credentials: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static("public"));
-
-app.use(routers);
+// app.use(routers);
 
 const path = "/api/v2/auth/token/get";
 
 const params = {
-    shop_id: 37698,
+    shop_id: "748211e597685173da3e",
     code: signature.code,
     partner_id: signature.partner_id,
+    // partner_id: "748211e597685173da3e",
     sign: signature.sign,
     timestamp: signature.timestamp,
 };
 const options = {
-    method: "GET",
+    method: "POST",
     url: `${signature.host}${path}`,
     params,
 };
 
 // axios
 //     .request(options)
-//     .then(function (response) {
+//     .then((response) => {
 //         console.log(response);
 //     })
-//     .catch(function (error) {
+//     .catch((error) => {
 //         console.error(error);
 //     });
 
--- web-server/src/utils/signature.js
+++ web-server/src/utils/signature.js
@@ -1,5 +1,5 @@
 const { createHmac } = require("crypto");
-var store = require("store");
+const mongoose = require("mongoose");
 
 // const host = "https://partner.shopeemobile.com";
 const host = "https://partner.test-stable.shopeemobile.com";
@@ -12,17 +12,68 @@ const path = "/api/v2/shop/auth_partner";
 const partner_key =
     "170ddadd5bd5ae843c7ce110a883066061a12f2fa5384091edcd16d202c4a02b";
 
-const timestamp = Math.round(Date.now() / 1000);
+let timestamp = Math.round(Date.now() / 1000);
+
 const baseString = `${partner_id}${path}${timestamp}`;
-const sign = createHmac("sha256", partner_key).update(baseString).digest("hex");
+let sign = createHmac("sha256", partner_key).update(baseString).digest("hex");
 const redirect = "https://naver.com";
 
 const code = "https://seller.test-stable.shopee.co.id";
 
-const url = `${host}${path}?partner_id=${partner_id}&redirect=${redirect}&timestamp=${timestamp}&sign=${sign}`;
+let url = `${host}${path}?partner_id=${partner_id}&redirect=${redirect}&timestamp=${timestamp}&sign=${sign}`;
 
-console.log(store.get("timestamp"));
-store.set("timestamp", timestamp);
-console.log(store.get("timestamp"));
+mongoose
+    .connect("mongodb://54.180.125.115:27017/admin", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        ignoreUndefined: true,
+        user: "test",
+        pass: "test",
+    })
+    .catch((err) => console.log(err));
+
+mongoose.connection.on("error", (err) => {
+    console.error("connect failed", err);
+});
+
+const timestampSchema = mongoose.Schema({
+    timestamp: "number",
+    sign: "string",
+    url: "string",
+});
+
+const TimestampSchema = mongoose.model("Schema", timestampSchema);
+
+const newTimestamp = new TimestampSchema({
+    timestamp: timestamp,
+    sign: sign,
+    url: url,
+});
+
+TimestampSchema.find((error, result) => {
+    timestamp - result[0].timestamp > 0 &&
+        console.log(
+            "Current authorization url expires after:",
+            300 - (timestamp - result[0].timestamp),
+            "s",
+        );
+    if (error) {
+        newTimestamp.save();
+    } else if (timestamp - result[0].timestamp >= 300) {
+        TimestampSchema.findById(result[0]._id, (error, data) => {
+            data.timestamp = timestamp;
+            data.sign = sign;
+            data.url = url;
+            data.save();
+        });
+        console.log("Saved!");
+    } else {
+        timestamp = result[0].timestamp;
+        sign = result[0].sign;
+        url = result[0].url;
+    }
+
+    console.log("timestamp:", timestamp, "\nsign:", sign, "\nurl:", url);
+});
 
 module.exports = { host, partner_id, redirect, timestamp, sign, url, code };
