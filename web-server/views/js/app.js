8211cfebb106f3906bf147d10e927e4760fa36b2
sun <sun@codebrick.co>
2022-01-06 13:52:07 +0900

    refactor

--- web-server/views/js/app.js
+++
@@ -1,8 +0,0 @@
-function generateLink(partnerid, secret, successReturnUrl) {
-    const token =  crypto.createHash('sha256').update(
-      successReturnUrl + secret,
-    ).digest('hex');
-    const url =
-      'https://partner.shopeemobile.com/api/v1/shop/auth_partner' +
-      `?id=${partnerid}&token=${token}&redirect=${successReturnUrl}`;
-    return url
