aca4ca19295991f29c77b33c792dec2e89b74c86
sun <sun@codebrick.co>
2022-01-07 12:43:57 +0900

    updated

--- signature.js
+++ signature.js
@@ -15,7 +15,7 @@ const partner_key =
 let timestamp = Math.round(Date.now() / 1000);
 
 const baseString = `${partner_id}${path}${timestamp}`;
-const redirect = "https://suml.xyz?code=123123";
+const redirect = "https://suml.xyz";
 let sign = createHmac("sha256", partner_key).update(baseString).digest("hex"); // = calauth
 
 const code = "6a626a4a4b494164494247617344724a";
