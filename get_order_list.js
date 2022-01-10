ea82f6e2a75032c7272ecd462ab84f7fe33e341a
sun <sun@codebrick.co>
2022-01-10 14:33:37 +0900

    통찰력

--- get_order_list.js
+++ get_order_list.js
@@ -8,10 +8,10 @@ const timestamp = Math.round(Date.now() / 1000);
 const partner_id = 1000639;
 const partner_key =
     "90e0bd35c8a63d24052d2acb14ee4ae98e2f7a76e7428c3fa6e9da941960cac0";
-const baseString = `${partner_id}${path}${timestamp}`;
-const sign = createHmac("sha256", partner_key).update(baseString).digest("hex");
-const shop_id = 37698;
 const access_token = "230d605ce3296648568fe64464e0c320";
+const shop_id = 37698;
+const baseString = `${partner_id}${path}${timestamp}${access_token}${shop_id}`;
+const sign = createHmac("sha256", partner_key).update(baseString).digest("hex");
 
 const params = {
     partner_id,
@@ -19,21 +19,21 @@ const params = {
     access_token,
     shop_id,
     sign,
-};
-const data = {
     time_range_field: "create_time",
     time_from: 1641786546,
     time_to: 1641786546,
+    page_size: 2,
     // page_size: 20,
     // order_state: "READY_TO_SHIP",
     // response_optional_fields: "order_status",
     shop_id,
 };
+// const data = {};
 const options = {
     method: "GET",
     url: `${host}${path}`,
     params,
-    data,
+    // data,
 };
 axios
     .request(options)
