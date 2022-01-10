be62707047949af91fbf7a6b7a852f6cdd57ef6e
sun <sun@codebrick.co>
2022-01-10 16:25:56 +0900

    test

--- get_order_detail.js
+++ get_order_detail.js
@@ -1,4 +1,5 @@
 const axios = require("axios").default;
+require("util").inspect.defaultOptions.depth = null;
 const { createHmac } = require("crypto");
 
 const host = "https://partner.test-stable.shopeemobile.com";
@@ -8,7 +9,7 @@ const timestamp = Math.round(Date.now() / 1000);
 const partner_id = 1000639;
 const partner_key =
     "90e0bd35c8a63d24052d2acb14ee4ae98e2f7a76e7428c3fa6e9da941960cac0";
-const access_token = "9be463e77e664cd3b1a62b0750e5c931";
+const access_token = "5f48f7bdba99c7461860ec4eac874577";
 const shop_id = 37698;
 const baseString = `${partner_id}${path}${timestamp}${access_token}${shop_id}`;
 const sign = createHmac("sha256", partner_key).update(baseString).digest("hex");
