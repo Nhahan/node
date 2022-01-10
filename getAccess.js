acd6446b48aff3aa3aa0747e3ac1a54dd29e9545
sun <sun@codebrick.co>
2022-01-10 12:56:31 +0900

    get_order_list

--- getAccess.js
+++ getAccess.js
@@ -23,13 +23,13 @@ app.use(express.json());
 
 const timestamp = Math.round(Date.now() / 1000);
 // const timestamp = 1641527426;
-const code = "584a4a52576a57694274796e6c4c6f61";
+const code = "6e596567485a635a7244616f4f745762";
 const partner_id = 1000639;
 const partner_key =
     "90e0bd35c8a63d24052d2acb14ee4ae98e2f7a76e7428c3fa6e9da941960cac0";
 const baseString = `${partner_id}${path}${timestamp}`;
 const sign = createHmac("sha256", partner_key).update(baseString).digest("hex");
-const shop_id = 37872;
+const shop_id = 37698;
 
 const params = {
     partner_id,
@@ -50,7 +50,6 @@ const options = {
 axios
     .request(options)
     .then((response) => {
-        console.log("GenerateAccessToken");
         console.log("response", response.data);
     })
     .catch((error) => {
@@ -58,3 +57,11 @@ axios
     });
 
 module.exports = app;
+
+// response {
+//     access_token: '6e6671794c5761576367584858435972',
+//     error: '',
+//     request_id: '2d1bf5c38b03a86cd00c5936ea296875',
+//     message: '',
+//     expire_in: 14379,
+//     refresh_token: '4548694e5a786353766d554a6d636f42'
