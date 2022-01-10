e0ffe2f9b9787cf284675af6c5458e866773d244
sun <sun@codebrick.co>
2022-01-10 17:12:06 +0900

    add_item

---
+++ add_item.js
@@ -0,0 +1,105 @@
+const axios = require("axios").default;
+require("util").inspect.defaultOptions.depth = null;
+const { createHmac } = require("crypto");
+
+const host = "https://partner.test-stable.shopeemobile.com";
+const path = "/api/v2/product/add_item";
+
+const timestamp = Math.round(Date.now() / 1000);
+const partner_id = 1000639;
+const partner_key =
+    "90e0bd35c8a63d24052d2acb14ee4ae98e2f7a76e7428c3fa6e9da941960cac0";
+const access_token = "fe346eff06b14803454d8d253918de71";
+const shop_id = 37698;
+const baseString = `${partner_id}${path}${timestamp}${access_token}${shop_id}`;
+const sign = createHmac("sha256", partner_key).update(baseString).digest("hex");
+
+const params = {
+    partner_id,
+    timestamp,
+    access_token,
+    shop_id,
+    sign,
+};
+
+const data = {
+    original_price: "555.5",
+    description: "This item is good!",
+    // weight,
+    item_name: "Good Item",
+    // item_status, // UNLIST, NORMAL
+    // dimension, // { "package_height": 11, "package_length": 11, "package_width": 11 }
+    normal_stock: "33",
+    logistic_info: [
+        {
+            size_id: 0,
+            shipping_fee: 23.12,
+            enabled: true,
+            logistic_id: 80101,
+            is_free: false,
+        },
+    ],
+    // attribute_list: [
+    //     {
+    //         attribute_id: 4990,
+    //         attribute_value_list: [
+    //             {
+    //                 value_id: 32142,
+    //                 original_value_name: "Brand",
+    //                 value_unit: " kg",
+    //             },
+    //         ],
+    //     },
+    // ],
+    category_id: 100143,
+    image: {
+        image_id_list: [],
+    },
+    // pre_order: {
+    //     "is_pre_order": false,
+    //     "days_to_ship": 3
+    // },
+    // item_sku,
+    // condition,
+    // wholesale: [
+    //     {
+    //         min_count: 1,
+    //         max_count: 100,
+    //         unit_price: 28.3,
+    //     },
+    // ],
+    // video_upload_id,
+    // brand: {
+    //     brand_id: 0,
+    //     original_brand_name: "nike",
+    // },
+    // item_dangerous: 0,
+    // tax_info: {
+    //     invoice_option: "",
+    //     vat_rate: "",
+    //     hs_code: "1234",
+    //     tax_code: "",
+    // },
+    // complaint_policy: {
+    //     warranty_time: "",
+    //     exclude_entrepreneur_warranty: "",
+    //     complaint_address_id: 0,
+    //     additional_information: "",
+    // },
+};
+
+const options = {
+    method: "POST",
+    url: `${host}${path}`,
+    params,
+    data,
+};
+
+axios
+    .request(options)
+    .then((response) => {
+        console.log("response", response.data);
+    })
+    .catch((error) => {
+        console.error("error", error.response.data);
+    });
---
+++ get_dts_limit.js
@@ -0,0 +1,40 @@
+const axios = require("axios").default;
+require("util").inspect.defaultOptions.depth = null;
+const { createHmac } = require("crypto");
+
+const host = "https://partner.test-stable.shopeemobile.com";
+const path = "/api/v2/product/get_dts_limit";
+
+const timestamp = Math.round(Date.now() / 1000);
+const partner_id = 1000639;
+const partner_key =
+    "90e0bd35c8a63d24052d2acb14ee4ae98e2f7a76e7428c3fa6e9da941960cac0";
+const access_token = "33c3945fbb0d772d1cbb4122dd7ac79e";
+const shop_id = 37698;
+const baseString = `${partner_id}${path}${timestamp}${access_token}${shop_id}`;
+const sign = createHmac("sha256", partner_key).update(baseString).digest("hex");
+
+const params = {
+    partner_id,
+    timestamp,
+    access_token,
+    shop_id,
+    sign,
+    category_id: 100143,
+};
+
+const options = {
+    method: "GET",
+    url: `${host}${path}`,
+    params,
+};
+
+axios
+    .request(options)
+    .then((response) => {
+        console.log("get_order_list");
+        console.log("response", response.data);
+    })
+    .catch((error) => {
+        console.error("get_order_list error", error.response.data);
+    });
