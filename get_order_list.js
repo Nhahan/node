90a3b7c534a410eac7a85fa391cf74df080e0714
sun <sun@codebrick.co>
2022-01-10 12:58:04 +0900

    get_order_list

---
+++ get_order_list.js
@@ -0,0 +1,46 @@
+const axios = require("axios").default;
+const { createHmac } = require("crypto");
+
+const host = "https://partner.test-stable.shopeemobile.com";
+const path = "/api/v2/order/get_order_list";
+
+const timestamp = Math.round(Date.now() / 1000);
+const partner_id = 1000639;
+const partner_key =
+    "90e0bd35c8a63d24052d2acb14ee4ae98e2f7a76e7428c3fa6e9da941960cac0";
+const baseString = `${partner_id}${path}${timestamp}`;
+const sign = createHmac("sha256", partner_key).update(baseString).digest("hex");
+const shop_id = 37698;
+const access_token = "7351725a77515177566c4a635a626c4c";
+
+const params = {
+    partner_id,
+    timestamp,
+    access_token,
+    shop_id,
+    sign,
+};
+const data = {
+    time_range_field: "create_time",
+    time_from: 1641786546,
+    time_to: 1641786546,
+    page_size: 20,
+    order_state: "UNPAID",
+    response_optional_fields: "order_status",
+    shop_id,
+};
+const options = {
+    method: "GET",
+    url: `${host}${path}`,
+    params,
+    data,
+};
+axios
+    .request(options)
+    .then((response) => {
+        console.log("get_order_list");
+        console.log("response", response.data);
+    })
+    .catch((error) => {
+        console.error("get_order_list error", error.response);
+    });
---
+++ refresh.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const axios = require("axios").default;
+const { createHmac } = require("crypto");
+
+// const refresh_token = "4548694e5a786353766d554a6d636f42";
+const refresh_token = "29dadab442aa8e522e86899bf91891db";
+const path = "/api/v2/auth/access_token/get";
+
+const host = "https://partner.test-stable.shopeemobile.com";
+const timestamp = Math.round(Date.now() / 1000);
+const partner_id = 1000639;
+const partner_key =
+    "90e0bd35c8a63d24052d2acb14ee4ae98e2f7a76e7428c3fa6e9da941960cac0";
+const baseString = `${partner_id}${path}${timestamp}`;
+const sign = createHmac("sha256", partner_key).update(baseString).digest("hex");
+const shop_id = 37872;
+
+const params = {
+    partner_id,
+    sign,
+    timestamp,
+};
+const data = {
+    refresh_token,
+    partner_id,
+    shop_id,
+};
+const options = {
+    method: "POST",
+    url: `${host}${path}`,
+    params,
+    data,
+};
+axios
+    .request(options)
+    .then((response) => {
+        console.log("RefreshAccessToken");
+        console.log("response", response.data);
+    })
+    .catch((error) => {
+        console.error("refresh error", error.response);
+    });
