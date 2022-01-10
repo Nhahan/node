c1e69a4f4cfc51d516e1fba55b91127575e5774f
sun <sun@codebrick.co>
2022-01-10 16:59:07 +0900

    get

--- get_category.js
+++ get_category.js
@@ -3,24 +3,24 @@ const { createHmac } = require("crypto");
 
 // const host = "https://partner.test-stable.shopeemobile.com";
 const host = "https://partner.test-stable.shopeemobile.com";
-const path = "/api/v2/order/get_category";
+const path = "/api/v2/product/get_category";
 
 const timestamp = Math.round(Date.now() / 1000);
 const partner_id = 1000639;
 const partner_key =
     "90e0bd35c8a63d24052d2acb14ee4ae98e2f7a76e7428c3fa6e9da941960cac0";
-const access_token = "d0044009b475d06e503b02b4d3747b02";
+const access_token = "76ba41404adae31a602e85a2f29a7868";
 const shop_id = 37698;
 const baseString = `${partner_id}${path}${timestamp}${access_token}${shop_id}`;
 const sign = createHmac("sha256", partner_key).update(baseString).digest("hex");
 
 const params = {
-    language: "id",
-    timestamp,
     partner_id,
+    timestamp,
     access_token,
     shop_id,
     sign,
+    language: "id",
 };
 
 const options = {
