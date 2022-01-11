287c1a46adbdf80c73268bf390162afe46a3345a
sun <sun@codebrick.co>
2022-01-11 15:40:52 +0900

    socket

---
+++ get_profile.js
@@ -0,0 +1,40 @@
+const axios = require("axios").default;
+require("util").inspect.defaultOptions.depth = null;
+const { createHmac } = require("crypto");
+
+const host = "https://partner.test-stable.shopeemobile.com";
+const path = "/api/v2/shop/get_profile";
+
+const timestamp = Math.round(Date.now() / 1000);
+const partner_id = 1000639;
+const partner_key =
+    "90e0bd35c8a63d24052d2acb14ee4ae98e2f7a76e7428c3fa6e9da941960cac0";
+const access_token = "6b555742434f724f4164555377637959";
+const shop_id = 37698;
+const baseString = `${partner_id}${path}${timestamp}${access_token}${shop_id}`;
+const sign = createHmac("sha256", partner_key).update(baseString).digest("hex");
+
+const params = {
+    partner_id,
+    timestamp,
+    access_token,
+    shop_id,
+    sign,
+};
+
+const options = {
+    method: "GET",
+    url: `${host}${path}`,
+    params,
+};
+
+axios
+    .request(options)
+    .then((response) => {
+        console.log("status", response.status);
+        console.log("headers", response.headers);
+        console.log("response", response.data);
+    })
+    .catch((error) => {
+        console.error("error", error.response.data);
+    });
---
+++ js/app.js
@@ -0,0 +1,14 @@
+// const socket = io.connect("https://suml.xyz:3000");
+const socket = io.connect("http://localhost:3000");
+
+socket.on("msg", function (data) {
+    $("#chatContent").append(`${data}<br>`);
+});
+
+$("#myChat").on("keyup", function () {
+    if (window.event.keyCode == 13) {
+        $("#chatContent").append(`Client : "${$(this).val()}" 보냅니다.<br>`);
+        socket.emit("msg", $(this).val());
+        $(this).val("");
+    }
+});
