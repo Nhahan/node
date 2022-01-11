16b703dc1023fd1943a12e51c40cdeed490d3c08
sun <sun@codebrick.co>
2022-01-11 10:22:48 +0900

    delete py

---
+++ get_acess_token.js
@@ -0,0 +1,50 @@
+const axios = require("axios").default;
+const { createHmac } = require("crypto");
+
+const host = "https://partner.test-stable.shopeemobile.com";
+const path = "/api/v2/auth/token/get";
+const timestamp = Math.round(Date.now() / 1000);
+// const timestamp = 1641527426;
+const code = "59516a7a4d646d76664964795a62516e";
+const partner_id = 1000639;
+const partner_key =
+    "90e0bd35c8a63d24052d2acb14ee4ae98e2f7a76e7428c3fa6e9da941960cac0";
+const baseString = `${partner_id}${path}${timestamp}`;
+const sign = createHmac("sha256", partner_key).update(baseString).digest("hex");
+const shop_id = 37698;
+
+const params = {
+    partner_id,
+    sign,
+    timestamp,
+};
+
+const data = {
+    code,
+    partner_id,
+    shop_id,
+};
+
+const options = {
+    method: "POST",
+    url: `${host}${path}`,
+    params,
+    data,
+};
+
+axios
+    .request(options)
+    .then((response) => {
+        console.log("response", response.data);
+    })
+    .catch((error) => {
+        console.error("error", error.response.data);
+    });
+
+// response {
+//     access_token: '6e6671794c5761576367584858435972',
+//     error: '',
+//     request_id: '2d1bf5c38b03a86cd00c5936ea296875',
+//     message: '',
+//     expire_in: 14379,
+//     refresh_token: '4548694e5a786353766d554a6d636f42'
---
+++ get_channel_list.js
@@ -0,0 +1,47 @@
+const axios = require("axios").default;
+require("util").inspect.defaultOptions.depth = null;
+const { createHmac } = require("crypto");
+
+const host = "https://partner.test-stable.shopeemobile.com";
+const path = "/api/v2/logistics/get_channel_list";
+
+const timestamp = Math.round(Date.now() / 1000);
+const partner_id = 1000639;
+const partner_key =
+    "90e0bd35c8a63d24052d2acb14ee4ae98e2f7a76e7428c3fa6e9da941960cac0";
+const access_token = "61596e7269577141636441485641724b";
+const shop_id = 37698;
+const baseString = `${partner_id}${path}${timestamp}${access_token}${shop_id}`;
+const sign = createHmac("sha256", partner_key).update(baseString).digest("hex");
+
+const params = {
+    partner_id,
+    timestamp,
+    access_token,
+    shop_id,
+    sign,
+};
+
+const data = {
+    partner_id,
+    shop_id,
+    timestamp,
+};
+
+const options = {
+    method: "GET",
+    url: `${host}${path}`,
+    params,
+    data,
+};
+
+axios
+    .request(options)
+    .then((response) => {
+        console.log("status", response.status);
+        console.log("headers", response.headers);
+        console.log("response", response.data);
+    })
+    .catch((error) => {
+        console.error("error", error.response.data);
+    });
