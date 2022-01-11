85be5540c09639de14f1d9f35385ba0e4eaa4ed8
sun <sun@codebrick.co>
2022-01-11 10:43:45 +0900

    server

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const signature = require("./signature");
 const fs = require("fs");
 const http = require("http");
 const https = require("https");
-http.createServer(app).listen(3000);
-// const optionHttps = {
-//     ca: fs.readFileSync("/etc/letsencrypt/live/suml.xyz/fullchain.pem"),
-//     key: fs.readFileSync("/etc/letsencrypt/live/suml.xyz/privkey.pem"),
-//     cert: fs.readFileSync("/etc/letsencrypt/live/suml.xyz/cert.pem"),
-// };
-// https.createServer(optionHttps, app).listen(443);
+// http.createServer(app).listen(3000);
+const optionHttps = {
+    ca: fs.readFileSync("/etc/letsencrypt/live/suml.xyz/fullchain.pem"),
+    key: fs.readFileSync("/etc/letsencrypt/live/suml.xyz/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/suml.xyz/cert.pem"),
+};
+https.createServer(optionHttps, app).listen(443);
 
 app.set("view engine", "hbs");
 
