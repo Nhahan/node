75d03fc3dfaba3296a95b5f7953484d548b7ce37
Nhahan <kisy324@naver.com>
2024-10-20 22:30:27 +0900

    `gitignore`

---
+++ deprecated2/src/infrastructure/express/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import { setupApp } from './setup/setup-app';
+import { setupAsyncErrors } from './setup/setup-async-errors';
+import { setupGlobalMiddlewares } from './setup/setup-global-middlewares';
+import { setupRoutes } from './setup/setup-routes';
+
+export const app = express();
+
+setupApp(app);
+setupGlobalMiddlewares(app);
+setupRoutes(app);
+setupAsyncErrors(app); // It has to be placed after all routes and middlewares
+
+// Port can be configured in .env file
+const port = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => {
+    console.log(`Server listening -> http://127.0.0.1:${port}`);
+  });
+}
