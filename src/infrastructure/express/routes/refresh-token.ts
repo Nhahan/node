ea94c407c8db3254ceeaa63be1d8324086fd4a6d
Nhahan <kisy324@naver.com>
2024-10-20 22:29:15 +0900

    deprecated2

--- src/infrastructure/express/routes/refresh-token.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { refreshUserControllerFactory } from '~/main/factories/controllers/token/refresh-token-controller-factory';
-import { expressRouteAdapter } from '../adapters/express-route-adapter';
-
-export const refreshTokenRoutes = Router();
-
-const { refreshTokenController } = refreshUserControllerFactory();
-
-refreshTokenRoutes.post('/', expressRouteAdapter(refreshTokenController));
